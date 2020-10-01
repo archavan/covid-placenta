dd7409acdc5ed1440bab4ccb934ddbbb9c55b815
archavan <c.arun13@gmail.com>
2020-10-01 13:34:39 -0400

    Figures for the paper

---
+++ code/99_paper-figures.R
@@ -0,0 +1,37 @@
+# 2020-10-01
+# Arun Chavan
+
+### packages ==================================================================
+library(tidyverse)
+
+# single cell
+library(Seurat)
+
+# plotting
+library(patchwork)
+library(ggthemes)
+library(ggrepel)
+library(ggforce) # for sina plots
+library(cowplot)
+library(ggtext)
+library(ungeviz) # for geom_hpline
+
+### data ======================================================================
+## Seurat
+seur <- readRDS("results/02_annotation/seurat-object_annotated.rds")
+
+## DE genes
+
+## Interferome output
+
+## GO output
+
+### UMAP ======================================================================
+
+### DE dotplot ================================================================
+
+### Interferome plot ==========================================================
+
+### GO plot ===================================================================
+
+### Putting it all together ===================================================
