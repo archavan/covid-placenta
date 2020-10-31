7469b90998b731dd2ed10c71047cbf42011011f1
archavan <c.arun13@gmail.com>
2020-10-31 12:30:17 -0400

    Redid annotation supp fig

--- code/99_paper_supp-fig_annotation.R
+++ code/99_paper_supp-fig_annotation.R
@@ -6,6 +6,7 @@ library(Seurat)
 
 library(patchwork) # plotting
 library(cowplot)
+library(ggrepel)
 
 ### data ======================================================================
 seur <- readRDS("results/02_annotation/seurat-object_annotated.rds")
@@ -40,10 +41,7 @@ cormat <- reshape2::melt(cor.matrix, na.rm = T)
 cormat$Var1_source <- sapply(strsplit(as.character(cormat$Var1), split = "_"), "[[", 1)
 cormat$Var2_source <- sapply(strsplit(as.character(cormat$Var2), split = "_"), "[[", 1)
 
-# subset
-# cormat <- cormat[which(cormat$Var1_source %in% refdata & 
-#                          cormat$Var2_source == "clust"), ]
-
+# calculate top 3 matches
 cormat$top3 <- NA
 
 for(i in unique(cormat$Var2)){
@@ -67,8 +65,8 @@ plot.dat <- cormat %>%
   filter(Var1_source %in% c("pavli", "surya", "vento"), 
          Var2_source %in% c("clust"))
 
-plot.dat$lab <- ann$annotation_merged[match(x = plot.dat$Var2, 
-                                            table = ann$seurat_clusters)]
+plot.dat$lab <- ann$annotation[match(x = plot.dat$Var2, 
+                                     table = ann$seurat_clusters)]
 plot.dat$lab[!(plot.dat$Var1 %in% c("vento_HB"))] <- NA # this is a lazy hack to make geom_text add label only once. If this was filtered to Var1_source == vento, it would add 35 (number of vento clusters) labels on top of each other.
 
 facet.names <- c(
@@ -79,17 +77,22 @@ facet.names <- c(
 
 cor.plot <- ggplot(plot.dat, aes(Var1, Var2, fill = value)) +
   geom_tile(colour = "white") +
-  scale_fill_gradient(low = 'white', high = 'red',
+  scale_fill_gradient(low = 'white', high = '#d94801',
                       name = "Spearman\nCorrelation",
-                      limits = c(0.40, 0.95),
-                      breaks = c(0.40, 0.60, 0.80),
-                      labels = c("0.4", "0.6", "0.8")) +
+                      limits = c(0.50, 0.95),
+                      oob = scales::squish,
+                      breaks = c(0.50, 0.7, 0.9),
+                      labels = c("<0.5", "0.7", "0.9")) +
   geom_point(aes(Var1, Var2, alpha = top3),
              size = 1, shape = 19, stroke  = 0) +
   scale_alpha_manual(values = c(1, 0.5, 0.25), 
                      breaks = c(1, 2, 3),
                      name = "Top 3 match", na.value = 0) +
-  facet_grid(. ~ Var1_source, scales = "free_x", space = "free_x",
+  guides(alpha = guide_legend(override.aes = list(size = 2)),
+         fill = guide_colorbar(ticks.colour = "black")) +
+  facet_grid(. ~ Var1_source, 
+             scales = "free_x", 
+             space = "free_x",
              labeller = as_labeller(facet.names)) +
   coord_cartesian(clip = "off") +
   geom_text(aes(label = lab), x = 33, size = 5.25/.pt, 
@@ -99,77 +102,137 @@ cor.plot <- ggplot(plot.dat, aes(Var1, Var2, fill = value)) +
   theme(
     aspect.ratio = 35,
     plot.tag = element_text(size = 8, colour = "black", face = 2),
-    strip.text = element_text(size = 7, color = "black"),
+    strip.text = element_text(size = 6, color = "black"),
     strip.background = element_blank(),
-    axis.text.x = element_text(angle = 90, size = 5.25, 
+    axis.text.x = element_text(angle = 90, size = 5, 
                                hjust = 1, vjust = 0.5, color = "black"),
-    axis.text.y = element_text(size = 5.25, color = "black"),
+    axis.text.y = element_text(size = 5, color = "black"),
     axis.ticks = element_line(size = 0.25),
     axis.ticks.length = unit(0.15, units = c('lines')),
-    axis.title = element_text(size = 7),
+    axis.title = element_text(size = 6),
     panel.grid = element_blank(),
-    panel.border = element_blank(),
+    panel.border = element_rect(size = 0.25, color = "black", fill = NA),
     legend.box.spacing = unit(2.5, "lines"),
-    legend.key.size = unit(0.65, units = c('lines')), 
-    legend.title = element_text(size = 6),
-    legend.text = element_text(size = 6)
+    legend.key.size = unit(0.5, units = c('lines')), 
+    legend.title = element_text(size = 5),
+    legend.text = element_text(size = 5)
   )
 
-### UMAP figure, optional =====================================================
-theme.umap <-   theme_classic() +
-  theme(axis.line = element_line(size = 0.25),
-        axis.ticks = element_line(size = 0.25),
-        axis.text = element_text(size = 6),
-        axis.title = element_text(size = 6),
-        plot.tag = element_text(size = 8, face = 2),
-        legend.position = "none",
-        aspect.ratio = 1)
+### UMAP figures ==============================================================
+## colors for plotting --------------------------------------------------------
+stack.colors.20 <- c("#6cb9a0",
+                     "#a90090",
+                     "#2abb3e",
+                     "#4140c3",
+                     "#a6d468",
+                     "#ff8dee",
+                     "#00b56e",
+                     "#b70048",
+                     "#39ddbe",
+                     "#e8591a",
+                     "#02aded",
+                     "#ba5600",
+                     "#68d6e3",
+                     "#ff6db4",
+                     "#00ba9d",
+                     "#ff8f94",
+                     "#4e5719",
+                     "#ffaf52",
+                     "#b27b5e",
+                     "#e2c36e",
+                     "#836400") # colors from https://medialab.github.io/iwanthue/
 
-# all annotations
-Idents(seur) <- "annotation"
-umap.full <- DimPlot(seur, label = TRUE, label.size = 6/.pt, shuffle = TRUE, repel = TRUE) + 
-  labs(tag = "B") +
-  theme.umap
-umap.full$layers[[2]]$geom_params$segment.size <- 0.25
+stack.colors.35 <- c("#b8bd38",
+                     "#a454cc",
+                     "#67be40",
+                     "#cc3fa5",
+                     "#4fc16f",
+                     "#dd74d2",
+                     "#4c8e2c",
+                     "#5b69d8",
+                     "#e0a736",
+                     "#824b9e",
+                     "#91b143",
+                     "#b786dd",
+                     "#888c2d",
+                     "#d9417f",
+                     "#5fc7a8",
+                     "#d4403f",
+                     "#48b1da",
+                     "#e1722b",
+                     "#4e65a6",
+                     "#ac7c21",
+                     "#8396de",
+                     "#ad522d",
+                     "#339580",
+                     "#a05084",
+                     "#4b9054",
+                     "#dc88b5",
+                     "#2a6a45",
+                     "#d97578",
+                     "#566926",
+                     "#a14456",
+                     "#a5b670",
+                     "#e6926c",
+                     "#7e7336",
+                     "#cea864",
+                     "#956534")
 
-# merged annotations
-Idents(seur) <- "annotation_merged"
-umap.merged <- DimPlot(seur, label = TRUE, label.size = 6/.pt, shuffle = TRUE, repel = TRUE) + 
-  labs(tag = "C") +
-  theme.umap
-umap.merged$layers[[2]]$geom_params$segment.size <- 0.25
+## function -------------------------------------------------------------------
+plot_umap <- function(object, idents, colors, tag) {
+  
+  # set idents
+  Idents(object) <- idents
+  
+  # extract plotting data
+  gg <- DimPlot(object, label = TRUE, shuffle = TRUE)
+  udat <- gg$data # point coordinates
+  ulab <- gg$layers[[2]]$data # label coordinates
+  
+  # new colors
+  ulab <- ulab[order(as.character(ulab$ident)), ]
+  ulab$newcolors <- colors
+  
+  # factors
+  udat$ident <- factor(udat$ident, levels = ulab$ident)
+  
+  # plot
+  p <- ggplot() +
+    geom_point(data = udat, 
+               aes(x = UMAP_1, y = UMAP_2, color = ident),
+               size = 0.1) +
+    scale_color_manual(values = alpha(ulab$newcolors, 1), 
+                       breaks = ulab$ident) +
+    labs(x = "UMAP 1", y = "UMAP 2", tag = tag) +
+    geom_label_repel(data = ulab, 
+                     aes(x = UMAP_1, y = UMAP_2, label = ident),
+                     size = 5/.pt,
+                     segment.size = 0.2, segment.color = "black",
+                     force = 1,
+                     fontface = 1, 
+                     label.padding = unit(0.1, "lines"), 
+                     label.r = 0,
+                     label.size = NA, # https://stackoverflow.com/questions/43417514/getting-rid-of-border-in-pdf-output-for-geom-label-for-ggplot2-in-r
+                     seed = 0, 
+                     fill = alpha(c("white"), 0.7)) +
+    theme_classic() +
+    theme(
+      aspect.ratio = 1,
+      plot.tag = element_text(face = 2, size = 8, color = "black"),
+      legend.position = "none",
+      axis.text = element_blank(),
+      axis.title = element_text(size = 5),
+      axis.line = element_line(size = 0.25),
+      axis.ticks = element_blank()
+    )
 
-# test umap plot with custom colours used in the stacked bar plot so that the colours match between plots
-stack.colors <- c("#6cb9a0",
-                  "#a90090",
-                  "#2abb3e",
-                  "#4140c3",
-                  "#a6d468",
-                  "#ff8dee",
-                  "#00b56e",
-                  "#b70048",
-                  "#39ddbe",
-                  "#e8591a",
-                  "#02aded",
-                  "#ba5600",
-                  "#68d6e3",
-                  "#ff6db4",
-                  "#00ba9d",
-                  "#ff8f94",
-                  "#4e5719",
-                  "#ffaf52",
-                  "#b27b5e",
-                  "#e2c36e",
-                  "#836400") # colors from https://medialab.github.io/iwanthue/
-Idents(seur) <- "annotation_merged"
-umap.merged1 <- DimPlot(seur, label = TRUE, label.size = 6/.pt, shuffle = TRUE, repel = TRUE, cols = stack.colors) + 
-  labs(tag = "C") +
-  theme.umap
-umap.merged1$layers[[2]]$geom_params$segment.size <- 0.25
-ggsave(
-  filename = "results/99_paper-figures/supp-fig_annotation/umap_colour-test.png",
-  umap.merged1, width = 3.5, height = 3.5, units = "in", type = "cairo"
-)
+}
+
+## plot: all annotations ------------------------------------------------------
+umap.full <- plot_umap(object = seur, idents = "annotation", colors = stack.colors.35, tag = "B")
+
+## plot: merged annotations ---------------------------------------------------
+umap.merged <- plot_umap(object = seur, idents = "annotation_merged", colors = stack.colors.20, tag = "C")
 
 ### arrange ===================================================================
 layout <- c(area(t = 1, l = 1, b = 45, r = 70),
@@ -180,48 +243,15 @@ composite <- cor.plot + umap.full + umap.merged +
   plot_layout(design = layout, guides = "keep")
   
 cowplot::ggsave2(
-  filename = "results/99_paper-figures/supp-fig_annotation/supp-fig_annotation.pdf",
+  filename = "results/99_paper-figures/supp-fig_annotation/supp-fig_annotation_composite.pdf",
   composite, 
   device = "pdf", width = 7, height = 7, units = "in"
 )
 
 cowplot::ggsave2(
-  filename = "results/99_paper-figures/supp-fig_annotation/supp-fig_annotation.png",
+  filename = "results/99_paper-figures/supp-fig_annotation/supp-fig_annotation_composite.png",
   composite, 
   width = 7, height = 7, units = "in", type = "cairo", dpi = 600
 )
 
-### write associated supplementary files ======================================
-## cluster annotations --------------------------------------------------------
-ann <- ann[order(ann$seurat_clusters), ]
-write.csv(ann, "results/99_paper-supp-files/cluster_annotations.csv", row.names = FALSE)
-
-## Cluster marker genes -------------------------------------------------------
-# marker genes are output from FindAllMarkers function, for all clusters, top genes upto logFC of 0.25. 
-markers <- read.csv("results/02_annotation/files/markers_sct.csv")
-
-# rename clusters from 0, 1 to clust_00, clust_01 etc.
-markers$cluster <- paste0("clust_", markers$cluster)
-markers$cluster <- gsub("(clust_)(\\d)$", "\\10\\2", markers$cluster)
-
-# rearrange columns
-markers <- markers[, c("cluster", "gene", "pct.1", "pct.2", "avg_logFC", "p_val", "p_val_adj")]
-
-# filter by adjusted p value
-markers <- markers %>% dplyr::filter(p_val_adj < 0.05)
-
-# write
-write.csv(markers, "results/99_paper-supp-files/cluster_marker-genes.csv", row.names = FALSE)
-
-## Number of cells of each celltype by control and covid status ---------------
-cellnum <- table(seur$annotation_merged, seur$covid) %>% as.data.frame()
-cellnum <- pivot_wider(data = cellnum, id_cols = "Var1", names_from = "Var2", values_from = "Freq")
-cellnum <- dplyr::rename(cellnum, "celltype" = "Var1")
-
-write.csv(cellnum, "results/99_paper-supp-files/n-cells_by-celltype-and-covid-status.csv", row.names = FALSE)
-
-### end =======================================================================
-
-
-
-
+### end  ======================================================================
